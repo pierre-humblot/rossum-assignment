7b59fee1202df149c6b6f7244b26106f9c931da6
pierre.humblot <pierre.humblot.pro@gmail.com>
2022-03-01 04:06:12 +0100

    first commit

--- pdf_rendering_service/api/urls.py
+++ pdf_rendering_service/api/urls.py
@@ -1,21 +1,11 @@
-# from django.conf.urls import url
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('api/', include('app.api.urls')),
-# ]
-
-
-from django.urls import path
-# from django.conf import settings
-# from django.conf.urls.static import static
-
 
 from .views import (
     upload_pdf,
     status_pdf,
     view_page,
 )
+
+
 
 urlpatterns = [
     path('documents', upload_pdf),
